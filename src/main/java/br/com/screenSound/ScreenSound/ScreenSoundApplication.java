bd65dc8e789cb5a8f083673bd1c634dcb250fe0f
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2024-01-04 16:36:55 -0300

    *-*

--- src/main/java/br/com/screenSound/ScreenSound/ScreenSoundApplication.java
+++ src/main/java/br/com/screenSound/ScreenSound/ScreenSoundApplication.java
@@ -1,6 +1,8 @@
 package br.com.screenSound.ScreenSound;
 
 import br.com.screenSound.ScreenSound.principal.Principal;
+import br.com.screenSound.ScreenSound.repositorio.ArtistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Primary;
 @SpringBootApplication
 public class ScreenSoundApplication implements CommandLineRunner {
 
+	@Autowired
+	private ArtistaRepository repositorio;
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenSoundApplication.class, args);
 	}
@@ -16,7 +20,7 @@ public class ScreenSoundApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception
 	{
-		Principal principal = new Principal();
+		Principal principal = new Principal(repositorio);
 
 		principal.exibeMenu();
 	}
--- src/main/java/br/com/screenSound/ScreenSound/principal/Principal.java
+++ src/main/java/br/com/screenSound/ScreenSound/principal/Principal.java
@@ -1,10 +1,22 @@
 package br.com.screenSound.ScreenSound.principal;
 
+import br.com.screenSound.ScreenSound.models.Artista;
+import br.com.screenSound.ScreenSound.models.Musica;
+import br.com.screenSound.ScreenSound.models.TipoArtista;
+import br.com.screenSound.ScreenSound.repositorio.ArtistaRepository;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Principal {
 
+    private final ArtistaRepository repositorio;
     private Scanner input = new Scanner(System.in);
+    public Principal(ArtistaRepository repositorio)
+    {
+        this.repositorio = repositorio;
+    }
     public void exibeMenu()
     {
         var opcao = -1;
@@ -15,9 +27,7 @@ public class Principal {
                     [1] Cadastrar Artista
                     [2] Cadastrar Música
                     [3] Listar músicas  
-                    [4] Buscar músicas por artistas
-                    [5] Pesquisar dados sobre um cantor
-                                    
+                    [4] Buscar músicas por artistas                         
                     [0] Sair                                 
                     """;
 
@@ -42,10 +52,6 @@ public class Principal {
                     buscarMusicasPorArtista();
                     break;
 
-                case 5:
-                    pesquisarDadosCantor();
-                    break;
-
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -56,21 +62,63 @@ public class Principal {
         }
     }
 
-    private void cadastrarArtista() {
+    private void cadastrarArtista()
+    {
+        var cadastrarNovo = "S";
+
+        while (cadastrarNovo.equalsIgnoreCase("s"))
+        {
+            System.out.printf("Insira o nome do artista: ");
+            var nomeArtista = input.nextLine();
+            System.out.printf("Informe o tipo do artista (Solo|Dupla|Banda): ");
+            var tipo = input.nextLine();
+            TipoArtista tipoArtista = TipoArtista.valueOf(tipo.toUpperCase());
+
+            Artista artista = new Artista(nomeArtista, tipoArtista);
+
+            repositorio.save(artista);
+
+            System.out.printf("Cadastrar mais artistas?(S|N)");
+            cadastrarNovo = input.nextLine();
+        }
 
     }
 
      private void cadastrarMusica() {
+         System.out.printf("Insira o nome do artista para o cadastro da música:");
+         var nome = input.nextLine();
+         Optional<Artista> artista = repositorio.findByNomeContainingIgnoreCase(nome);
 
-    }
-    private void pesquisarDadosCantor() {
+         if(artista.isPresent())
+         {
+             System.out.printf("Informe o nome da música:");
+             var nomeMusica = input.nextLine();
+
+             Musica musica = new Musica(nomeMusica);
+
+             // one to many e many to one pede que mapeia dos 2 lados pra dar certo no BD2
+             artista.get().getMusicas().add(musica);
+             musica.setArtista(artista.get());
+
+             repositorio.save(artista.get());
+         }
+         else
+         {
+             System.out.println("Artista não encontrado.");
+         }
     }
 
     private void listarMusicas() {
+        List<Artista> artistas = repositorio.findAll();
+        artistas.forEach(System.out::println);
 
     }
     private void buscarMusicasPorArtista() {
+        System.out.println("Insira o nome do artista:");
+        var nomeArtista = input.nextLine();
+
+        List<Musica> musicas = repositorio.musicasPorArtista(nomeArtista);
+        musicas.forEach(System.out::println);
         
     }
-
 }
